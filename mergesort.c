b131acd6ef91e43e5a8fe1330ded36c500fae906
ablaberge <annika.laberge@icloud.com>
2024-09-09 22:47:09 -0700

    Basic structure of methods needed

---
+++ mergesort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> //Necessary to generate random numbers
+
+void mergeSort(int *array, int size);
+int createArray(int size);
+
+int main(int argc, char **argv){
+    
+    int arraySize = atoi(argv[1]);
+    int *array = (int*) malloc(arraySize*sizeof(int));
+    return 0;
+}
+
+int createArray(int size){
+     
+}
+
+void mergeSort(int *array, int size) {
+ }
+
+
